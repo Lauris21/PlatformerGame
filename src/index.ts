ea1a7b8a224db0ed8580250a2448236662d0e7e0
Laura <laurasanchez0301@gmail.com>
2023-10-07 20:12:22 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,48 @@
-
 import Phaser from "phaser";
+import PlayScene from "./scenes/Play";
 
-const config: Phaser.Types.Core.GameConfig = {
+
+
+const width : number= 1200;
+const height: number = 600;
+const birdPosition = { x: width * 0.1, y: height / 2 };
+
+const sharedConfig = {
+  width: width,
+  height: height,
+  startPosition: birdPosition,
+};
+
+//Tienen que ir en orden de ejecución
+const Scenes = [PlayScene];
+
+// Creamos una nueva escena con la configuracion compartida
+const createScene = (Scene:any) => new Scene(sharedConfig);
+//Inicializamos instancias para las escenas
+const initScenes = () => Scenes.map(createScene);
+
+const config = {
+  //WebGL (web graphics library) API JS para renderizar gráficos en 2 y 3D
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+  ...sharedConfig,
+  scale: {
+    parent: "game-container",
+    mode: Phaser.Scale.FIT, // Escala para ajustar al contenedor
+    width: width,
+    height: height,
   },
-  scene: {
-    preload: preload,
-    create: create
-  }
+  // Evita que se vean manchas alrededor de elementos
+  pixelArt: true,
+  physics: {
+    //utiliza la física de arcade -> gestiona simulaciones físicas (gravedad, velocidad, etc)
+    default: "arcade",
+    arcade: {
+      // debug: true,  ayuda ver movimientos y a donde va el objeto
+    },
+  },
+  //La escena es lo que puede ver en la pantalla
+  //Tiene 3 funciones precargar, crear y actualizar
+  scene: initScenes(),
 };
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
