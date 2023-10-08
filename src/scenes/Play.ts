7f9b157ff104f454df73c77d352d3828c3102521
Laura <laurasanchez0301@gmail.com>
2023-10-08 12:48:56 +0200

    refactoring

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -1,19 +1,37 @@
 import Phaser from "phaser";
 
 class PlayScene extends Phaser.Scene {
+  constructor() {
+    super("PlayScene");
+  }
 
-    constructor() {
-        super("PlayScene");
-      }
+  create() {
+    const map: Phaser.Tilemaps.Tilemap = this.createMaps();
+    const layers = this.createLayers(map);
+  }
 
-    create() {
-const map = this.make.tilemap({key: "crystal_map"})
-const tileSet1 = map.addTilesetImage("main_lev_build_1", "main_lev_build_1")
-const tileSet2 = map.addTilesetImage("crystal_word_map", "main_lev_build_2")
+  createMaps() {
+    const map: Phaser.Tilemaps.Tilemap = this.make.tilemap({
+      key: "crystal_map",
+    });
+    map.addTilesetImage("main_lev_build_1", "main_lev_build_1");
+    return map;
+  }
 
-map.createStaticLayer("environment", tileSet1)
-map.createStaticLayer("platforms", tileSet1)
-    }
+  createLayers(map: Phaser.Tilemaps.Tilemap) {
+    const tileset: Phaser.Tilemaps.Tileset = map.getTileset("main_lev_build_1");
+    const environment: Phaser.Tilemaps.StaticTilemapLayer =
+      map.createStaticLayer("environment", tileset);
+    const platforms: Phaser.Tilemaps.DynamicTilemapLayer = map.createDynamicLayer(
+      "platforms",
+      tileset
+    );
+
+    return {
+      environment,
+      platforms,
+    };
+  }
 }
 
-export default PlayScene
+export default PlayScene;
