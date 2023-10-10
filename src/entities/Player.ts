b463eb5300dde290bd671433c9327b252787c84c
Laura <laurasanchez0301@gmail.com>
2023-10-11 00:23:31 +0200

    create player class

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -2,7 +2,39 @@ import PlayScene from "../scenes/Play"
 
 export class Player extends Phaser.Physics.Arcade.Sprite {
 
+    gravity: number
+    playerSpeed : number = 200
+    cursors : Phaser.Types.Input.Keyboard.CursorKeys 
+
     constructor(scene: PlayScene, x: number, y: number) {
         super(scene, x, y, "player")
+
+        scene.add.existing(this)
+        scene.physics.add.existing(this)
+
+        this.init()
+    }
+
+    init() {
+        this.gravity = 500
+        this.playerSpeed = 200
+        this.cursors = this.scene.input.keyboard.createCursorKeys()
+
+        this.setGravityY(this.gravity)
+        this.setCollideWorldBounds(true)
+    }
+
+    preUpdate(time: number, delta: number):void {
+        super.preUpdate(time, delta)
+        const { left, right} = this.cursors
+
+    if (left.isDown) {
+        this.setVelocityX(-this.playerSpeed)
+
+    } else if ( right.isDown) {
+        this.setVelocityX(this.playerSpeed)
+    } else {
+        this.setVelocityX(0)
+    }
     }
 }
--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -2,10 +2,10 @@ import Phaser from "phaser";
 import { Player } from "../entities/Player";
 
 class PlayScene extends Phaser.Scene {
-    cursors : Phaser.Types.Input.Keyboard.CursorKeys 
+   
     player: Player
 
-    playerSpeed : number = 200
+    
 
   constructor() {
     super("PlayScene");
@@ -16,11 +16,9 @@ class PlayScene extends Phaser.Scene {
 
     const layers = this.createLayers(map);
 
-   this.player = this.createPlayer()
+   this.createPlayer()
 
     this.physics.add.collider(this.player, layers.platformColliders)
-
-    this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   createMaps() {
@@ -59,24 +57,6 @@ class PlayScene extends Phaser.Scene {
 
   createPlayer() {
     this.player = new Player(this, 100, 250)
-    this.player = this.physics.add.sprite(100, 170, "player")
-    this.player.setGravityY(500)
-    this.player.setCollideWorldBounds(true)
-  
-    return this.player
-  }
-
-  update() {
-    const { left, right} = this.cursors
-
-    if (left.isDown) {
-        this.player.setVelocityX(-this.playerSpeed)
-
-    } else if ( right.isDown) {
-        this.player.setVelocityX(this.playerSpeed)
-    } else {
-        this.player.setVelocityX(0)
-    }
   }
 }
 
