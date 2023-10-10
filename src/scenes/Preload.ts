2f516a3505fb9e8805c0069341fe3ddc7e31df34
Laura <laurasanchez0301@gmail.com>
2023-10-11 01:32:57 +0200

    running animation

--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -1,22 +1,25 @@
 import Phaser from "phaser";
 
 class PreloadScene extends Phaser.Scene {
+  constructor() {
+    super("PreloadScene");
+  }
 
-    constructor() {
-        super("PreloadScene");
-      }
+  preload() {
+    this.load.tilemapTiledJSON("crystal_map", "assets/crystal_map.json");
+    this.load.image("main_lev_build_1", "assets/main_lev_build_1.png");
+    this.load.image("main_lev_build_2", "assets/main_lev_build_2.png");
 
-    preload() {
-this.load.tilemapTiledJSON("crystal_map", "assets/crystal_map.json")
-this.load.image("main_lev_build_1", "assets/main_lev_build_1.png")
-this.load.image("main_lev_build_2", "assets/main_lev_build_2.png")
+    this.load.spritesheet("player", "assets/player/move_sprite_1.png", {
+      frameWidth: 32,
+      frameHeight: 38,
+      spacing: 32,
+    });
+  }
 
-this.load.image("player", "assets/player/movements/idle01.png")
-    }
-
-    create() {
-this.scene.start("PlayScene")
-    }
+  create() {
+    this.scene.start("PlayScene");
+  }
 }
 
-export default PreloadScene
+export default PreloadScene;
