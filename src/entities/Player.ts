3b4803f95c57a65c1a8fbb72d16cf2ae614daaa5
Laura <laurasanchez0301@gmail.com>
2023-10-11 11:01:46 +0200

    jumping

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,52 +1,57 @@
-import PlayScene from "../scenes/Play"
-import initAnimation from "./playerAnims"
+import PlayScene from "../scenes/Play";
+import initAnimation from "./playerAnims";
 export class Player extends Phaser.Physics.Arcade.Sprite {
+  scene: PlayScene;
+  gravity: number;
+  playerSpeed: number = 200;
+  cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
-    gravity: number
-    playerSpeed : number = 200
-    cursors : Phaser.Types.Input.Keyboard.CursorKeys 
+  constructor(scene: PlayScene, x: number, y: number) {
+    super(scene, x, y, "player");
 
-    constructor(scene: PlayScene, x: number, y: number) {
-        super(scene, x, y, "player")
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
 
-        scene.add.existing(this)
-        scene.physics.add.existing(this)
+    this.init();
+    this.initEvents();
+  }
 
-        this.init()
-        this.initEvents()
-    }
+  init() {
+    this.gravity = 500;
+    this.playerSpeed = 200;
+    this.cursors = this.scene.input.keyboard.createCursorKeys();
 
-    init() {
-        this.gravity = 500
-        this.playerSpeed = 200
-        this.cursors = this.scene.input.keyboard.createCursorKeys()
+    this.setGravityY(this.gravity);
+    this.setCollideWorldBounds(true);
 
-        this.setGravityY(this.gravity)
-        this.setCollideWorldBounds(true)
+    initAnimation(this.scene.anims);
+  }
 
-        initAnimation(this.scene.anims)
-    }
+  initEvents() {
+    this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
+  }
 
-    initEvents() {
-        this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this)
-    }
+  update(time: number, delta: number): void {
+    const { left, right, space, up } = this.cursors;
 
-    update(time: number, delta: number):void {
-        super.preUpdate(time, delta)
-        const { left, right} = this.cursors
+    const onFloor = (this.body as Phaser.Physics.Arcade.Body).onFloor()
 
     if (left.isDown) {
-        this.setVelocityX(-this.playerSpeed)
-        this.setFlipX(true)
-
-    } else if ( right.isDown) {
-        this.setVelocityX(this.playerSpeed)
-        this.setFlipX(false)
+      this.setVelocityX(-this.playerSpeed);
+      this.setFlipX(true);
+    } else if (right.isDown) {
+      this.setVelocityX(this.playerSpeed);
+      this.setFlipX(false);
     } else {
-        this.setVelocityX(0)
+      this.setVelocityX(0);
     }
 
-    this.body.velocity.x !== 0 ? this.play("run", true): this.play("idle", true)
-     
+    if ((space.isDown || up.isDown) && onFloor) {
+      this.setVelocityY(-this.playerSpeed * 1.5);
     }
+
+    this.body.velocity.x !== 0
+      ? this.play("run", true)
+      : this.play("idle", true);
+  }
 }
