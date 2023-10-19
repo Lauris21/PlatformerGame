9542a70d4195610bb0e8608c5521d301cfd486e4
Laura <laurasanchez0301@gmail.com>
2023-10-19 15:58:53 +0200

    destroy proyectile

--- src/attacks/Projectile.ts
+++ src/attacks/Projectile.ts
@@ -2,6 +2,9 @@ import PlayScene from "../scenes/Play"
 
 class Projectile extends Phaser.Physics.Arcade.Sprite{
     speed : number;
+    maxDistance: number;
+    traveledDistance: number;
+
     constructor(scene : PlayScene, x: number, y : number, key : string) {
         super(scene, x, y, key);
 
@@ -9,6 +12,18 @@ class Projectile extends Phaser.Physics.Arcade.Sprite{
         scene.physics.add.existing(this)
 
         this.speed = 300
+        this.maxDistance = 200
+        this.traveledDistance = 0
+    }
+
+    preUpdate(time: number, delta: number): void {
+        super.preUpdate(time, delta)
+
+        this.traveledDistance += this.body.deltaAbsX()
+        // Eliminamos la bola cuando recorre 200 px
+        if(this.traveledDistance >= this.maxDistance) {
+            this.destroy()
+        }
     }
 
     fire() {
