c462ba3208b536540cea06b6aec8a16b90c7888d
Laura <laurasanchez0301@gmail.com>
2023-10-31 11:27:55 +0100

    add image to container

--- src/components/Hud.ts
+++ src/components/Hud.ts
@@ -3,7 +3,8 @@ import PlayScene from "../scenes/Play";
 export class Hud extends Phaser.GameObjects.Container {
   fontSize: number;
   lineHeight: number;
-  scoreBoard: Phaser.GameObjects.Text;
+  containerWidth: number;
+  scoreBoard: Phaser.GameObjects.Container;
   scoreBoard2: Phaser.GameObjects.Text;
   scoreBoard3: Phaser.GameObjects.Text;
 
@@ -13,7 +14,11 @@ export class Hud extends Phaser.GameObjects.Container {
     scene.add.existing(this);
 
     const { rightTopCorner } = scene.config;
-    this.setPosition(rightTopCorner.x - 75, rightTopCorner.y + 10);
+    this.containerWidth = 70;
+    this.setPosition(
+      rightTopCorner.x - this.containerWidth,
+      rightTopCorner.y + 10
+    );
     this.setScrollFactor(0);
 
     this.setupList();
@@ -22,10 +27,8 @@ export class Hud extends Phaser.GameObjects.Container {
   setupList() {
     this.fontSize = 20;
 
-    this.scoreBoard = this.scene.add.text(0, 0, "0", {
-      fontSize: `${this.fontSize}px`,
-      fill: "#fff",
-    });
+    this.createScore();
+
     this.scoreBoard2 = this.scene.add.text(0, 0, "Hola", {
       fontSize: `${this.fontSize}px`,
       fill: "#fff",
@@ -46,5 +49,19 @@ export class Hud extends Phaser.GameObjects.Container {
       );
       this.lineHeight += 20;
     });
+  }
+
+  createScore() {
+    const scoreText = this.scene.add.text(0, 0, "0", {
+      fontSize: `${this.fontSize}px`,
+      fill: "#fff",
+    });
+
+    const scoreImage = this.scene.add
+      .image(scoreText.width + 5, 0, "diamond")
+      .setOrigin(0)
+      .setScale(1.3);
+
+    this.scoreBoard = this.scene.add.container(0, 0, [scoreText, scoreImage]);
   }
 }
