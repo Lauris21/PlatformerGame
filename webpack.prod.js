967dae72a5eb36d0aff9dcd46a8b5e1c4299637a
Laura <laurasanchez0301@gmail.com>
2023-11-04 14:21:20 +0100

    webpack.prod fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path")
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,6 +10,12 @@ module.exports = merge(common, {
   performance: {
     maxEntrypointSize: 90000,
     maxAssetSize: 900000
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+    assetModuleFilename: "[path][name][ext]",
   },
   optimization: {
     minimizer: [
